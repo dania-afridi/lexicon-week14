3fca516e3756c99f00221a9d1ec28653683f83b4
dania-afridi <dania.awais33@gmail.com>
2024-04-03 10:39:37 +0200

    index.js modified.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 ///******* Tuesday Exercise ***********/
+
 // Ex # 01:
 let nonParamFunc = ()=>{ console.log("Bernadette had a boat");}
+nonParamFunc();
 
 // Ex # 02:
 let hadVehicle = (vehicle) => { console.log(`Bernadette had a ${vehicle}`);}
+hadVehicle("volvo");
 
+// Ex # 03:
+let calculateBalance = (balance, cost=0) => {
+    const newBalance = balance - cost ;
+    console.log(`Your new balance is [${newBalance}]Kr.`);
+}
+calculateBalance(500);
+calculateBalance(500, 30);
 
+// Ex # 04:
+let areaOfCircle = (radius) => { 
+    let area = radius*radius*Math.PI;
+    return area;
+}
+console.log(areaOfCircle(3));
 
+// Ex # 05:
+let threeTimes = (x, y, z)=> {
+    let sum = x + y + z;
+    return sum;
+}
+console.log(threeTimes(5, 25, 30));
+console.log(threeTimes(15, 3, " adding two numbers and one string."));
+console.log(threeTimes("Adding text and two numbers ", 15, 3));
+
+///******* Wednesday Exercise ***********/
+
+// Ex # 01:
 let weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 console.log(weekDays[3]);
+
+// Ex # 02:
