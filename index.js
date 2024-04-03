c30159a285a5c3cf2c9b474546cacff90490c185
dania-afridi <dania.awais33@gmail.com>
2024-04-03 10:12:09 +0200

    index.js modified.

---
+++ index.js
@@ -0,0 +1,11 @@
+///******* Tuesday Exercise ***********/
+// Ex # 01:
+let nonParamFunc = ()=>{ console.log("Bernadette had a boat");}
+
+// Ex # 02:
+let hadVehicle = (vehicle) => { console.log(`Bernadette had a ${vehicle}`);}
+
+
+
+let weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+console.log(weekDays[3]);
